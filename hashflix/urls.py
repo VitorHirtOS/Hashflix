a84ed5cda64ecdd7a9cd51c0ab011e3b399fcd5a
Vitor Mateus Hirt <zorinos.masterzorin@outlook.com>
2023-02-10 23:58:50 -0300

    upload filmes

--- hashflix/urls.py
+++ hashflix/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views
+from .views import Home, HomeFilmes, DetalhesFilmes
 
 
 urlpatterns = [
-    path('', views.Home, name="home-netflix"),
-    path('filmes/', views.HomeFilme, name="homefilmes")
+    path('', Home.as_view()),
+    path('filmes/', HomeFilmes.as_view()),
+    path('filmes/<int:pk>', DetalhesFilmes.as_view()),
 ]
